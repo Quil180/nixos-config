81c1f80fcba8e5daeff7bb14c4fe9c8a0861a1c2
Quil <quil180gaming@gmail.com>
2024-11-24 23:49:58 -0500

    got rid of redundant lines

--- personal/stylix/stylix.nix
+++ personal/stylix/stylix.nix
@@ -5,13 +5,5 @@
     enable = true;
     image = ../../wallpapers/eepy_myne.png;
     polarity = "dark";
-    homeManagerIntegration = {
-      autoImport = true;
-      followSystem = true;
-    };
-
-    targets.console.enable = true;
-    targets.nixvim.enable = true;
-
   };
 }
