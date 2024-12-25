e9b6a8b13ed9b6705438f899a950bf3c4ffa2f5b
Quil180 <quil180gaming@gmail.com>
2024-12-25 16:34:44 -0500

    modularized peristence

--- system/snowflake/configuration.nix
+++ system/snowflake/configuration.nix
@@ -17,6 +17,7 @@
     # Extra options...
     ../universal/g14.nix
     ../universal/sops.nix
+    ../universal/persist.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
@@ -70,7 +71,6 @@
     users.quil = {
       isNormalUser = true;
       hashedPasswordFile = config.sops.secrets.quil-password.path;
-      # hashedPasswordFile = config.sops.secrets.quil-password.path;
       extraGroups = [
         "networkmanager"
         "wheel"
@@ -99,52 +99,6 @@
     # printing support via CUPS
     printing.enable = true;
   };
-
-  # system files we want to keep
-  fileSystems."/persist".neededForBoot = true;
-  environment.persistence."/persist/system" = {
-    hideMounts = true;
-    directories = [
-      "/etc/nixos"
-      "/var/log"
-      "/var/lib/bluetooth"
-      "/var/lib/nixos"
-      "/var/lib/systemd/coredump"
-      "/var/lib/sops-nix"
-      "/etc/NetworkManager/system-connections"
-      {
-        directory = "/var/lib/colord";
-        user = "colord";
-        group = "colord";
-        mode = "u=rwx,g=rx,o=";
-      }
-    ];
-  };
-
-  boot.initrd.postDeviceCommands = lib.mkAfter ''
-    mkdir /btrfs_tmp
-    mount /dev/root_vg/root /btrfs_tmp
-    if [[ -e /btrfs_tmp/root ]]; then
-        mkdir -p /btrfs_tmp/old_roots
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-    fi
-
-    delete_subvolume_recursively() {
-        IFS=$'\n'
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
-        done
-        btrfs subvolume delete "$1"
-    }
-
-    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-        delete_subvolume_recursively "$i"
-    done
-
-    btrfs subvolume create /btrfs_tmp/root
-    umount /btrfs_tmp
-  '';
 
   nixpkgs.config = {
     allowUnfree = true;
