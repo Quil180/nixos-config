ca74b85ad8856b652b9583dc519dcfea0e34ff96
Quil <quil180gaming@gmail.com>
2024-12-24 19:42:29 -0500

    aight, github secrets now work

--- users/universal/sops.nix
+++ users/universal/sops.nix
@@ -1,17 +1,22 @@
 { inputs, ... }:
-{
+let
+  USER = "quil";
+in {
   imports = [
     inputs.sops-nix.homeManagerModules.sops
   ];
 
   sops = {
-    age.keyFile = "/home/quil/.config/sops/age/keys.txt";
+    age.keyFile = "/home/${USER}/.config/sops/age/keys.txt";
     defaultSopsFile = ../../secrets.yaml;
     validateSopsFiles = false;
 
     secrets = {
-      "private_keys/quil" = {
-        path = "/home/quil/.ssh/id_private";
+      "private_keys/${USER}" = {
+        path = "/home/${USER}/.ssh/id_ed25519";
+      };
+      "public_keys/${USER}" = {
+        path = "/home/${USER}/.ssh/id_ed22519.pub";
       };
     };
   };
