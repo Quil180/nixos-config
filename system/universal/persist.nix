429f4f35d388b43b7f4fabdef158583384cb97dc
Quil <quil180gaming@gmail.com>
2025-03-17 23:12:51 -0400

    made libvirt virtual machines persist every reboot

--- system/universal/persist.nix
+++ system/universal/persist.nix
@@ -12,6 +12,7 @@
       "/var/lib/bluetooth"
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
+      "/var/lib/libvirt"
       {
         directory = "/var/lib/colord";
         user = "colord";
