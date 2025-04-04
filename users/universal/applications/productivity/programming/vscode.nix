238491e96c089478abd8a794ad6e6d225c57ac97
Quil <quil180gaming@gmail.com>
2025-04-04 18:04:37 -0400

    updated vscode

--- users/universal/applications/productivity/programming/vscode.nix
+++ users/universal/applications/productivity/programming/vscode.nix
@@ -1,12 +1,12 @@
 {pkgs, ...}: {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
       vscodevim.vim
       yzhang.markdown-all-in-one
       ms-vscode.cpptools-extension-pack
-      teros-technology.teroshdl
+      github.copilot
     ];
   };
 }
