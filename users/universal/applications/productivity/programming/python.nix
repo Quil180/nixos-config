f8f7b60c4a341a4ceb912edbf27110209ec82883
Quil <quil180gaming@gmail.com>
2025-04-05 04:37:08 -0400

    added python, not compiling yet

--- users/universal/applications/productivity/programming/python.nix
+++ users/universal/applications/productivity/programming/python.nix
@@ -1,13 +1,21 @@
-{
-  config,
-  pkgs,
-  ...
-}: let
+{pkgs, ...}: let
   pythonEnv = pkgs.python3.withPackages (ps:
     with ps; [
-      vunit-hdl
+      (ps.buildPythonPackage rec {
+        pname = "vunit-hdl";
+        version = "4.7.0"; # replace with the desired version
+        src = ps.fetchPypi {
+          inherit pname version;
+        };
+      })
       edalize
-      yowasp-yosys
+      (ps.buildPythonPackage rec {
+        pname = "yowasp-yosys";
+        version = "0.51.0.0"; # replace with the desired version
+        src = ps.fetchPypi {
+          inherit pname version;
+        };
+      })
       cocotb
     ]);
 in {
