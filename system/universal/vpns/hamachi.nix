b3ffd25fbb813d2f6ebda2214d80365fb79cba67
Quil <quil180gaming@gmail.com>
2025-04-05 05:27:08 -0400

    trying potential fix for bug

--- system/universal/vpns/hamachi.nix
+++ system/universal/vpns/hamachi.nix
@@ -2,6 +2,7 @@
   environment.systemPackages = with pkgs; [
     haguichi
     logmein-hamachi
+    bind.dnsutils
   ];
   services.logmein-hamachi.enable = true;
   programs.haguichi.enable = true;
