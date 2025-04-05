2e7700d9deb9c907d54ff124ac73d049cb633685
Quil <quil180gaming@gmail.com>
2025-04-05 15:30:05 -0400

    got discord working again

--- users/universal/applications/social/discord.nix
+++ users/universal/applications/social/discord.nix
@@ -10,110 +10,101 @@
   programs.nixcord = {
     enable = true;
     discord = {
-      package = pkgs.discord-canary;
+      package = pkgs.discord;
       vencord.enable = true;
     };
     quickCss = ''
-        /**
-       * @name AOB Theme
+      /**
+       * @name ClearVision V7 for BetterDiscord
        * @author ClearVision Team
-       * @version 6.9.0
-       * @description Highly customizable and beautiful theme for BetterDiscord.
-       * @source https://github.com/ClearVision/ClearVision-v6
+       * @version 7.0.0
+       * @description Highly customizable theme for BetterDiscord.
+       * @source https://github.com/ClearVision/ClearVision-v7
        * @website https://clearvision.github.io
        * @invite dHaSxn3
-       * @BDEditor Clear Vision
-      */
-
-      @import url('https://clearvision.github.io/ClearVision-v6/main.css');
-      @import url('https://discordstyles.github.io/RadialStatus/dist/RadialStatus.css');
-
+       */
+      /* IMPORT CSS */
+      @import url("https://clearvision.github.io/ClearVision-v7/main.css");
+      @import url("https://clearvision.github.io/ClearVision-v7/betterdiscord.css");
+      /* SETTINGS */
       :root {
+        /* ACCENT COLORS */
         --main-color: #A36240;
         --hover-color: #773D1E;
         --success-color: #43b581;
         --danger-color: #982929;
-        --url-color: #AD8200;
-        --background-image: url('https://images8.alphacoders.com/122/1223658.jpg');
-        --background-shading: 100%;
-        --background-position: center;
-        --background-size: cover;
-        --background-repeat: no-repeat;
-        --background-attachment: fixed;
-        --background-brightness: 100%;
-        --background-contrast: 100%;
-        --background-saturation: 100%;
-        --background-grayscale: 0%;
-        --background-invert: 0%;
-        --background-blur: 2px;
-        --background-overlay: rgba(0,0,0,0.6);
-        --user-popout-image: url('https://images8.alphacoders.com/122/1223658.jpg');
-        --user-popout-position: center;
-        --user-popout-size: cover;
-        --user-popout-repeat: no-repeat;
-        --user-popout-attachment: fixed;
-        --user-popout-brightness: 100%;
-        --user-popout-contrast: 100%;
-        --user-popout-saturation: 100%;
-        --user-popout-grayscale: 0%;
-        --user-popout-invert: 0%;
-        --user-popout-blur: 6px;
-        --user-modal-image: url('https://images8.alphacoders.com/122/1223658.jpg');
-        --user-modal-position: center;
-        --user-modal-size: cover;
-        --user-modal-repeat: no-repeat;
-        --user-modal-attachment: fixed;
-        --user-modal-brightness: 100%;
-        --user-modal-contrast: 100%;
-        --user-modal-saturation: 100%;
-        --user-modal-grayscale: 0%;
-        --user-modal-invert: 0%;
-        --user-modal-blur: 3px;
-        --home-icon: transparent;
-        --home-position: center;
-        --home-size: 40px;
-        --channel-unread: #FFC107;
-        --channel-color: rgba(255,255,255,0.3);
-        --muted-color: rgba(255,255,255,0.1);
+        /* STATUS COLORS */
         --online-color: #43b581;
         --idle-color: #faa61a;
         --dnd-color: #982929;
         --streaming-color: #593695;
         --offline-color: #808080;
-        --main-font: gg sans;
-        --code-font: Consolas;
-        --channels-width: 220px;
-        --members-width: 240px;
-        --backdrop-overlay: rgba(0,0,0,0.8);
-        --backdrop-image: var(--background-image);
-        --backdrop-position: var(--background-position);
-        --backdrop-size: var(--background-size);
-        --backdrop-repeat: var(--background-repeat);
-        --backdrop-attachment: var(--background-attachment);
-        --backdrop-brightness: var(--background-brightness);
-        --backdrop-contrast: var(--background-contrast);
-        --backdrop-saturation: var(--background-saturation);
-        --backdrop-invert: var(--background-invert);
-        --backdrop-grayscale: var(--background-grayscale);
-        --backdrop-sepia: var(--background-sepia);
-        --backdrop-blur: var(--background-blur);
-        --bd-blue: var(--main-color);
-        --bd-blue-hover: var(--hover-color);
-        --bd-blue-active: var(--hover-color);
-        --rs-small-spacing: 3px;
-        --rs-medium-spacing: 3px;
-        --rs-large-spacing: 3px;
-        --rs-small-width: 3px;
-        --rs-medium-width: 3px;
-        --rs-large-width: 3px;
-        --rs-avatar-shape: 50%;
-        --rs-online-color: #43b581;
-        --rs-idle-color: #faa61a;
-        --rs-dnd-color: #f04747;
-        --rs-offline-color: #636b75;
-        --rs-streaming-color: #643da7;
-        --rs-phone-visible: block;
+        /* APP BACKGROUND */
+        --background-shading-percent: 100%;
+        --background-image: url(https://images8.alphacoders.com/122/1223658.jpg);
+        --background-attachment: fixed;
+        --background-filter: saturate(calc(var(--saturation-factor, 1) * 1));
+        /* USER POPOUT BACKGROUND */
+        --user-popout-image: var(--background-image);
+        --user-popout-attachment: var(--background-attachment);
+        --user-popout-filter: var(--background-filter);
+        /* USER MODAL BACKGROUND */
+        --user-modal-image: var(--background-image);
+        --user-modal-attachment: var(--background-attachment);
+        --user-modal-filter: var(--background-filter);
+        /* HOME ICON */
+        --home-icon: url(https://clearvision.github.io/icons/discord.svg);
+        /* FONTS */
+        --main-font: "gg sans", Whitney, "Helvetica Neue", Helvetica, Arial, sans-serif;
+        --code-font: Consolas, "gg mono", "Liberation Mono", Menlo, Courier, monospace;
       }
+
+      /* THEME SPECIFIC SHADING */
+      /* LIGHT THEME */
+      :is(.theme-light, .theme-dark .theme-light) {
+        --background-shading: rgba(252, 252, 252, 0.3);
+        --card-shading: rgba(252, 252, 252, 0.3);
+        --popout-shading: rgba(252, 252, 252, 0.7);
+        --modal-shading: rgba(252, 252, 252, 0.5);
+        --input-shading: rgba(0, 0, 0, 0.3);
+        --normal-text: #36363c;
+        --muted-text: #75757e;
+      }
+
+      /* ASH THEME */
+      :is(.theme-dark, .theme-light .theme-dark) {
+        --background-shading: rgba(0, 0, 0, 0.4);
+        --card-shading: rgba(0, 0, 0, 0.2);
+        --popout-shading: rgba(0, 0, 0, 0.6);
+        --modal-shading: rgba(0, 0, 0, 0.4);
+        --input-shading: rgba(255, 255, 255, 0.05);
+        --normal-text: #d8d8db;
+        --muted-text: #aeaeb4;
+      }
+
+      /* DARK THEME */
+      :is(.theme-darker, .theme-light .theme-darker) {
+        --background-shading: rgba(0, 0, 0, 0.6);
+        --card-shading: rgba(0, 0, 0, 0.3);
+        --popout-shading: rgba(0, 0, 0, 0.7);
+        --modal-shading: rgba(0, 0, 0, 0.5);
+        --input-shading: rgba(255, 255, 255, 0.05);
+        --normal-text: #fbfbfb;
+        --muted-text: #94949c;
+      }
+
+      /* ONYX THEME */
+      :is(.theme-midnight, .theme-light .theme-midnight) {
+        --background-shading: rgba(0, 0, 0, 0.8);
+        --card-shading: rgba(0, 0, 0, 0.4);
+        --popout-shading: rgba(0, 0, 0, 0.8);
+        --modal-shading: rgba(0, 0, 0, 0.6);
+        --input-shading: rgba(255, 255, 255, 0.05);
+        --normal-text: #dcdcde;
+        --muted-text: #86868e;
+      }
+
+      /* ADD ADDITIONAL CSS BELOW HERE */
     '';
     config = {
       useQuickCss = true;
