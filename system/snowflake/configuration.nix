889ea702584aecc30aad531bfaa3f449ea166215
Quil <quil180gaming@gmail.com>
2025-04-19 15:40:38 -0400

    updated deletion of unneeded packages to 1w

--- system/snowflake/configuration.nix
+++ system/snowflake/configuration.nix
@@ -145,7 +145,7 @@
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 2w";
+      options = "--delete-older-than 1w";
     };
     settings = {
       auto-optimise-store = true;
