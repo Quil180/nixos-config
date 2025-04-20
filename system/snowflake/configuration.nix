6f6a7b7dd83bb83ab45d7c6366f5114cd03dd536
Quil <quil180gaming@gmail.com>
2025-04-20 13:30:55 -0400

    updated some stuff

--- system/snowflake/configuration.nix
+++ system/snowflake/configuration.nix
@@ -42,7 +42,9 @@
   boot = {
     loader = {
       systemd-boot.enable = false;
-      efi.canTouchEfiVariables = true;
+      efi = {
+        canTouchEfiVariables = true;
+      };
       grub = {
         enable = true;
         device = "nodev";
