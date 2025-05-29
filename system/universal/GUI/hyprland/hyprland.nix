3d4e9a0127951ba927bca8ca3b49a6df1a2b1aa6
Quil <quil180gaming@gmail.com>
2025-05-28 21:25:20 -0400

    added x11 server and mangohud/mangohud customizer

--- system/universal/GUI/hyprland/hyprland.nix
+++ system/universal/GUI/hyprland/hyprland.nix
@@ -16,13 +16,14 @@
   };
 
   services = {
-
     # setting sddm as default login screen
     displayManager.sddm = {
       enable = true;
       enableHidpi = true;
       autoNumlock = true;
     };
+    # for any applications that require native X11
+    xserver.enable = true;
   };
   security.polkit.enable = true;
   environment.sessionVariables = {
