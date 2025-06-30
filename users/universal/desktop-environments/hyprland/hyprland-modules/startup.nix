a18571413617624a924d54b1c60463b7bc19eb84
Quil <quil180gaming@gmail.com>
2025-06-30 16:56:59 -0400

    updated stuff

--- users/universal/desktop-environments/hyprland/hyprland-modules/startup.nix
+++ users/universal/desktop-environments/hyprland/hyprland-modules/startup.nix
@@ -2,17 +2,18 @@
   dotfiles = "~/.dotfiles";
 in {
   wayland.windowManager.hyprland.settings.exec-once = [
-    "swww-daemon"
-    "swwww img ${dotfiles}/wallpapers/wallpaper.jpg"
-    "nm-applet --indicator"
-    "asusctl -c 80"
-    "asusctl led-mode static -c ffffff"
-    "wl-paste --watch cliphist store"
-    "dunst"
-    "xwaylandvideobridge" # for xwayland screensharing
-    "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-    "/usr/lib/polkit-kde-authentication-agent-1"
-    "hyprctl setcursor rose-pine-hyprcursor 24"
-    "rog-control-center"
+    "bash -c 'sleep 1 && swww-daemon'"
+    "bash -c 'sleep 1 && swwww img ${dotfiles}/wallpapers/wallpaper.jpg'"
+    "bash -c 'sleep 1 && nm-applet --indicator'"
+    "bash -c 'sleep 1 && asusctl -c 80'"
+    "bash -c 'sleep 1 && asusctl led-mode static -c ffffff'"
+    "bash -c 'sleep 1 && wl-paste --watch cliphist store'"
+    "bash -c 'sleep 1 && dunst'"
+    "bash -c 'sleep 1 && xwaylandvideobridge'" # for xwayland screensharing
+    "bash -c 'sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP --all'"
+    "bash -c 'sleep 1 && /usr/lib/polkit-kde-authentication-agent-1'"
+    "bash -c 'sleep 1 && hyprctl setcursor rose-pine-hyprcursor 24'"
+    "bash -c 'sleep 1 && rog-control-center'"
+    "bash -c 'sleep 1 && waybar'"
   ];
 }
