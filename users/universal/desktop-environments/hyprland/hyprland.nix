a25beb354847cd210bc6e8739c80f4e96094208e
Quil <quil180gaming@gmail.com>
2025-12-27 15:38:33 -0500

    fixed bug

--- users/universal/desktop-environments/hyprland/hyprland.nix
+++ users/universal/desktop-environments/hyprland/hyprland.nix
@@ -104,7 +104,6 @@
     enable = true;
     extraPortals = [
       pkgs.xdg-desktop-portal-gtk
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
     ];
     config = {
       common = {
