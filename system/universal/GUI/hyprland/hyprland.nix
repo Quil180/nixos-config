e6bfe77519d035a2b3b8489f2e5d2e3bd6fed868
Quil180 <quil180gaming@gmail.com>
2026-05-07 14:57:53 -0400

    yup

--- system/universal/GUI/hyprland/hyprland.nix
+++ system/universal/GUI/hyprland/hyprland.nix
@@ -1,53 +1,58 @@
-{ topConfig, lib, pkgs, ... }:
 {
-  flake.nixosModules.hyprland = 
-{ pkgs, inputs, ... }:
+  topConfig,
+  lib,
+  pkgs,
+  ...
+}:
 {
-  fonts.packages = with pkgs; [
-    iosevka
-  ];
+  flake.nixosModules.hyprland =
+    { pkgs, inputs, ... }:
+    {
+      fonts.packages = with pkgs; [
+        iosevka
+      ];
 
-  programs.hyprland = {
-    enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-    xwayland = {
-      enable = true;
-    };
-  };
-
-  security.polkit.enable = true;
-  environment.sessionVariables = {
-    XDG_CURRENT_DESKTOP = "Hyprland";
-    ELECTRON_OZONE_PLATFORM_HINT = "wayland";
-    NIXOS_OZONE_WL = "1";
-    WLR_NO_HARDWARE_CURSORS = "1";
-    AQ_NO_HARDWARE_CURSORS = "1";
-    AQ_DRM_DEVICES = "/dev/dri/card2:/dev/dri/card2";
-  };
-
-  # enabling xdg
-  xdg.portal = {
-    enable = true;
-    xdgOpenUsePortal = false;
-    config = {
-      common = {
-        default = [ "gtk" ];
-        "org.freedesktop.portal.OpenURI" = [ "gtk" ];
+      programs.hyprland = {
+        enable = true;
+        package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+        portalPackage =
+          inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+        xwayland = {
+          enable = true;
+        };
       };
-      hyprland = {
-        default = [
-          "gtk"
-          "hyprland"
+
+      security.polkit.enable = true;
+      environment.sessionVariables = {
+        XDG_CURRENT_DESKTOP = "Hyprland";
+        ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+        NIXOS_OZONE_WL = "1";
+        WLR_NO_HARDWARE_CURSORS = "1";
+        AQ_NO_HARDWARE_CURSORS = "1";
+        AQ_DRM_DEVICES = "/dev/dri/card2:/dev/dri/card2";
+        MOX_ENABLE_WAYLAND = "1";
+      };
+
+      # enabling xdg
+      xdg.portal = {
+        enable = true;
+        xdgOpenUsePortal = false;
+        config = {
+          common = {
+            default = [ "gtk" ];
+            "org.freedesktop.portal.OpenURI" = [ "gtk" ];
+          };
+          hyprland = {
+            default = [
+              "gtk"
+              "hyprland"
+            ];
+            "org.freedesktop.portal.OpenURI" = [ "gtk" ];
+          };
+        };
+        extraPortals = with pkgs; [
+          xdg-desktop-portal-gtk
         ];
-        "org.freedesktop.portal.OpenURI" = [ "gtk" ];
       };
     };
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-    ];
-  };
-}
-;
 }
